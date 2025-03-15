ebc831f8303df4a1020ca02969fc5a12aa3283a3
realChrisDeBon <christopherdebon@gmail.com>
2025-03-14 20:51:41 -0500

    * adding ExampleApplication and peer LAN network test TODO: documentation for LAN network test

---
+++ ExampleApplication/Program.cs
@@ -0,0 +1,77 @@
+﻿using P2PNet;
+using P2PNet.Peers;
+using P2PNet.NetworkPackets;
+using static P2PNet.PeerNetwork;
+using P2PNet.Routines;
+using System.Diagnostics;
+
+namespace ExampleApplication
+{
+    internal class Program
+    {
+        static List<int> targerPorts;
+        static void Main(string[] args)
+        {
+
+            if (args.Length > 0)
+            {
+                targerPorts = args.ToArray().Select(x => int.Parse(x)).ToList();
+                PeerNetwork.DesignatedPorts = targerPorts;
+                PeerNetwork.BroadcasterPort = targerPorts[0]; // we'll chain these together
+            }
+            
+            PeerNetwork.LoadLocalAddresses();
+
+            PeerNetwork.IncomingPeerTrustPolicy.IncomingPeerPlacement = IncomingPeerTrustPolicy.IncomingPeerMode.EventBased;
+            PeerNetwork.IncomingPeerTrustPolicy.RunDefaultTrustProtocol = true;
+            PeerNetwork.IncomingPeerTrustPolicy.AllowDefaultCommunication = true;
+
+
+            PeerNetwork.PeerAdded += NewPeerAdded;
+            PeerNetwork.IncomingPeerConnectionAttempt += NewPeerChannel;
+
+            PeerNetwork.BeginAcceptingInboundPeers();
+            PeerNetwork.StartBroadcastingLAN();
+            
+            Console.WriteLine("Hello, p2p world!");
+            Console.WriteLine($"Listening port: {ListeningPort}, local IP: {PublicIPV4Address}");
+            Task.Run(() => { WaitForPeers(); });
+            while (true)
+            {
+                Console.ReadLine();
+            }
+        }
+
+        private static void WaitForPeers() 
+        {
+            while (true)
+            {
+                if(TrustedPeerChannels.Count >= 1)
+                {
+                    PeerNetwork.StopBroadcastingLAN();
+                    Console.WriteLine($"Active peer count: {ActivePeerChannels.Count}");
+                    Console.WriteLine($"Active peer addresses: {string.Join(", ", ActivePeerChannels.Select(x => x.peer.Address.ToString()))}");
+
+                    foreach (var peer in ActivePeerChannels)
+                    {
+                        peer.LoadOutgoingData(new PureMessagePacket("Thanks for being my peer!"));
+                    }
+                    break;
+                }
+                Thread.Sleep(500);
+            }
+        }
+
+        private static void NewPeerChannel(object? sender, IncomingPeerEventArgs e)
+        {
+            Console.WriteLine($"New peer address: {e.Peer.Address.ToString()}");
+        }
+
+        private static void NewPeerAdded(object? sender, NewPeerEventArgs e)
+        {
+            e.peerChannel.LoadOutgoingData(new PureMessagePacket("Hello fellow peer!"));
+        }
+
+
+    }
+}
